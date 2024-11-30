30521254ab99a814dd18b9e62d8a3ac72c6e60b0
김형석 <hayward_kim@gimhyeongseog-ui-MacBookPro.local>
2024-11-30 21:44:03 +0900

    feat: added default(fonts)

---
+++ src/styles/colors.tsx
@@ -0,0 +1,12 @@
+class Color {
+    static brand = "#FF4040";
+    static white = "#FFFFFF";
+    static black = "#1F1F1F";
+    static gray = "#DCDCDC";
+    static green = "#569650";
+    static gold = "#FFD700";
+    static silver = "#C0C0C0";
+    static bronze = "#CD7F32";
+}
+
+export default Color;
---
+++ src/styles/typography.tsx
@@ -0,0 +1,5 @@
+class TypoGraphy {
+
+}
+
+export default TypoGraphy;
